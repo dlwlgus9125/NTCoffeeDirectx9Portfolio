5ad265c2517b3502eac337e602da1d3b3cc1b979
준범 박 <ehddndpr@naver.com>
2017-06-21 17:03:25 +0900

    NPC_Material

--- TeamPortPolio/TeamPortPolio/cNpcManager.cpp
+++ TeamPortPolio/TeamPortPolio/cNpcManager.cpp
@@ -21,6 +21,7 @@ void cNpcManager::Init(std::vector<ST_NPC_INFO> vecNpc)
 		m_vecSphere[i].vCenter = m_vecNpc[i].pos;
 	}
 	D3DXCreateSphere(D3DDevice, 0.7f, 10, 10, &m_pMesh, NULL);
+	SetMtrl();
 }
 
 void cNpcManager::Render()
@@ -30,7 +31,19 @@ void cNpcManager::Render()
 		D3DXMATRIXA16 matT;
 		D3DXMatrixTranslation(&matT, m_vecSphere[i].vCenter.x, m_vecSphere[i].vCenter.y + 0.7f, m_vecSphere[i].vCenter.z);
 		D3DDevice->SetTransform(D3DTS_WORLD, &matT);
-		
+		D3DDevice->SetTexture(0, NULL);
+		D3DDevice->SetFVF(ST_PN_VERTEX::FVF);
+		D3DDevice->SetRenderState(D3DRS_LIGHTING, true);
+		if (i == 0 || i == 1 || i == 3 || i == 4)
+		{
+			m_mtrl.Ambient = D3DXCOLOR(1.0F, 0.0F, 0.0F, 1.0F);
+			D3DDevice->SetMaterial(&m_mtrl);
+		}
+		else
+		{
+			m_mtrl.Ambient = D3DXCOLOR(0.0F, 1.0F, 0.0F, 1.0F);
+			D3DDevice->SetMaterial(&m_mtrl);
+		}
 		m_pMesh->DrawSubset(0);
 	}
 }
@@ -54,5 +67,7 @@ void cNpcManager::Release()
 
 void cNpcManager::SetMtrl()
 {
-	
+	ZeroMemory(&m_mtrl, sizeof(D3DMATERIAL9));
+	m_mtrl.Diffuse = D3DXCOLOR(1.0F, 0.0F, 0.0F, 1.0F);
+	m_mtrl.Specular = D3DXCOLOR(1.0F, 0.0F, 0.0F, 1.0F);
 }
