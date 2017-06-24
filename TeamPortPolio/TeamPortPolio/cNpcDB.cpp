2293fdcb9cf3d78eb991dec1b24dc7195d9a8ce7
준범 박 <ehddndpr@naver.com>
2017-06-25 08:38:00 +0900

    맵 txt 변경....NPC 회전,,, 이름... 변경....

--- TeamPortPolio/TeamPortPolio/cNpcDB.cpp
+++ TeamPortPolio/TeamPortPolio/cNpcDB.cpp
@@ -5,12 +5,11 @@ void cNpcDB::Setup()
 {
 	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_WEAPON, "obj/NPC/Human/Weapon/", "Blacksmith.X"));
 	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_ARMOR, "obj/NPC/Human/Armor/", "Citizenmid.X"));
-	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_RECRUIT, "obj/NPC/Human/Recruit/", "Warriormedium.X"));
 	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_STUFF, "obj/NPC/Human/Stuff/", "FemalePeasant.X"));
 	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_INN, "obj/NPC/Human/Inn/", "FemaleMerchant.X"));
-
+	Add(new ST_Npc(N_R_HUMAN, N_C_HUMAN_RECRUIT, "obj/NPC/Human/Recruit/", "Warriormedium.X"));
+	
 	Add(new ST_Npc(N_R_ORC, N_C_ORC_WEAPON, "obj/NPC/Orc/Weapon/", "felorc_sword.X"));
-	Add(new ST_Npc(N_R_ORC, N_C_ORC_WEAPON, "obj/NPC/Orc/Armor/", "orcmalewarriorlight.X"));
 	Add(new ST_Npc(N_R_ORC, N_C_ORC_ARMOR, "obj/NPC/Orc/Armor/", "orcmalewarriorlight.X"));
 	Add(new ST_Npc(N_R_ORC, N_C_ORC_RECRUIT, "obj/NPC/Orc/Recruit/", "rexxar.X"));
 	Add(new ST_Npc(N_R_ORC, N_C_ORC_STUFF, "obj/NPC/Orc/Stuff/", "orcmalekidgruntling.X"));
