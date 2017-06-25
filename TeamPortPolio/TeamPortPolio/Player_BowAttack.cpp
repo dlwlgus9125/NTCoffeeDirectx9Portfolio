9a0b0949e17ea2f86756b99eddfbed0f9a35efa1
jihyoen lee <dlwlgus9125@naver.com>
2017-06-25 14:11:21 +0900

    .

--- TeamPortPolio/TeamPortPolio/Player_BowAttack.cpp
+++ TeamPortPolio/TeamPortPolio/Player_BowAttack.cpp
@@ -8,19 +8,20 @@ void Player_BowAttack::OnBegin(cPlayer* pPlayer)
 	pPlayer->GetBowSkin()->SetAnimationIndexBlend(BOW_PULL);
 	pPlayer->GetMesh()->SetAnimationIndexBlend(P_BOWATTACK1);
 	m_fPrevCameraDist = CAMERA->GetCameraDitance();
-	
+
+	if (SOUND->FindChannel("PullBow") == NULL)SOUND->Play("PullBow",2.0f);
 }
 
 void Player_BowAttack::OnUpdate(cPlayer* pPlayer, float deltaTime)
 {
 	SetCursor(nullptr);
 
-	
+
 
 
 	if ((P_STATE)pPlayer->GetMesh()->GetIndex() == P_BOWATTACK1)
 	{
-		CAMERA->SetCameraDistance(CAMERA->GetCameraDitance() - m_fPrevCameraDist/10);
+		CAMERA->SetCameraDistance(CAMERA->GetCameraDitance() - m_fPrevCameraDist / 10);
 		if (CAMERA->GetCameraDitance() < 2) CAMERA->SetCameraDistance(2);
 		if (pPlayer->GetMesh()->GetPassedTime() > pPlayer->GetMesh()->GetCurrentAnim()->GetPeriod() - 0.1f)
 		{
@@ -33,13 +34,14 @@ void Player_BowAttack::OnUpdate(cPlayer* pPlayer, float deltaTime)
 		pPlayer->FSM()->Play(PLAYER_STATE_IDLE);
 	}
 	else if (INPUT->IsMouseUp(MOUSE_LEFT))
-	{	
+	{
+		if (SOUND->FindChannel("ShootBow") == NULL)SOUND->Play("ShootBow", 2.0f);
 		pPlayer->GetBowSkin()->SetAnimationIndexBlend(BOW_PUSH);
 		pPlayer->GetMesh()->SetAnimationIndexBlend(P_BOWATTACK2);
 		pPlayer->IsPullBow(false);
 
 		OBJECT->AddPlayerArrow(pPlayer->GetCharacterEntity(), pPlayer->SetUpAim());
-		
+
 		CAMERA->SetCameraDistance(m_fPrevCameraDist);
 	}
 
